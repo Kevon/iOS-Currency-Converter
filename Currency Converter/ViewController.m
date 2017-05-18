d7fcb243ad0015af441a804c7acccd118a5efab5
Kevin Skompinski <kevons5252@gmail.com>
2017-05-17 20:11:26 -0400

    Initial Commit

--- Currency Converter/ViewController.m
+++ Currency Converter/ViewController.m
@@ -7,21 +7,50 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *InputField;
+@property (weak, nonatomic) IBOutlet UIButton *Button;
+@property (weak, nonatomic) IBOutlet UILabel *Currency1;
+@property (weak, nonatomic) IBOutlet UILabel *Currency2;
+@property (weak, nonatomic) IBOutlet UILabel *Currency3;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    self.Button.enabled = NO;
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
+    self.Button.enabled = YES;
+    
+    double inputValue = [self.InputField.text floatValue];
+    
+    double euroValue = inputValue * currencies.EUR;
+    double yenValue = inputValue * currencies.JPY;
+    double poundValue = inputValue * currencies.GBP;
+    
+    NSString *temp1 = [NSString stringWithFormat:@"%.2f", euroValue];
+    NSString *temp2 = [NSString stringWithFormat:@"%.2f", yenValue];
+    NSString *temp3 = [NSString stringWithFormat:@"%.2f", poundValue];
+    
+    self.Currency1.text = temp1;
+    self.Currency2.text = temp2;
+    self.Currency3.text = temp3;
+    
+    
+
 }
 
 @end
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'Currency Converter' do
+
+pod 'CurrencyRequest' , ‘~> 0.1'
+
+end
+
